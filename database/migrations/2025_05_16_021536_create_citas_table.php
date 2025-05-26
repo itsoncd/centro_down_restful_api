7bc04b99e987a11c987e665f10ce33bd6feb7e8b
Diego Robles <125101098+Castle373@users.noreply.github.com>
2025-05-26 13:33:46 -0700

    Fecha Hora Cita

--- database/migrations/2025_05_16_021536_create_citas_table.php
+++ database/migrations/2025_05_16_021536_create_citas_table.php
@@ -8,9 +8,8 @@ return new class extends Migration {
     public function up(): void {
         Schema::create('citas', function (Blueprint $table) {
             $table->id();
-
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade'); // Quien organiza la cita
-            $table->date('fecha_cita');
+            $table->dateTime('fecha_cita');
             $table->string('correo');
             $table->string('nombre_alumno');
             $table->string('nombre_tutor');
